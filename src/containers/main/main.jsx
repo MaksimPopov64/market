2ab5063c1d684fbb7fe42b3036a552705f4db225
Maksim_Popov <Maksim_Popov1@epam.com>
2020-09-11 10:35:19 +0400

    added search

--- src/containers/main/main.jsx
+++ src/containers/main/main.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Header } from '../../components/header/header';
+import _ from 'lodash';
 import { goods } from '../../mocks/goods';
+import { Header } from '../../components/header/header';
+
 import { amountSelector } from '../../store/cart/selectors';
 import { ProductsCollection } from '../products-collection/products-collection';
 
 const Main = props => {
     const goodsCount = useSelector(state => amountSelector(state));
+    const [goodsItems, filterGoods] = useState(goods);
+    const handleChange = val => {
+        if (val.length) {
+            filterGoods(goodsItems.filter(x =>
+                x.name.toLowerCase().includes(val.toLowerCase()) || x.id.toLowerCase().includes(val.toLowerCase())));
+        } else filterGoods(goods);
+    }
     return (
         <div>
             <Header title="Market" url="/cart" productsInCart={goodsCount} />
-            <ProductsCollection type="main" products={goods} />
+            <div style={{ paddingTop: '50px' }}>
+                <input type="text" placeholder="Search" onChange={(e) => _.debounce(handleChange(e.target.value))} />
+            </div>
+            <ProductsCollection type="main" products={goodsItems} />
         </div>);
 }
 
